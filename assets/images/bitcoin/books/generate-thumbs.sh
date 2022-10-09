111ff2393f02bb63990baefc3ec9a337c2080bcd
Gigi <dergigi@pm.me>
2022-10-09 13:56:06 +0200

    feat(books): add German covers

--- assets/images/bitcoin/books/generate-thumbs.sh
+++ assets/images/bitcoin/books/generate-thumbs.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-magick mogrify -resize "400x400>" -format webp -quality 69 -path ./ highres/*.jpg
-magick mogrify -resize "400x400>" -format jpg -quality 69 -path ./jpg highres/*.jpg
+magick mogrify -resize "400x400>" -format webp -quality 69 -path ./ highres/*.jpg || true
+magick mogrify -resize "400x400>" -format webp -quality 69 -path ./ highres/*.webp || true
+magick mogrify -resize "400x400>" -format webp -quality 69 -path ./ highres/*.png || true
+magick mogrify -resize "400x400>" -format jpg -quality 69 -path ./jpg *.webp
